c11dd9d4c44ee297bd3f7c22128b9b645395d32e
Mohammad Haider <arzhdr@gmail.com>
2024-06-24 17:44:53 -0400

    fixed errors in routers

--- app/api/route.js
+++
@@ -1,6 +0,0 @@
-// import { OpenAICleint, AzurekeyCredentials } from "@azure/openai";
-import { NextResponse } from "next/server";
-
-const endpoint = process.env.ENDPOINT;
-const apiKey = process.env.OPENAI_API_KEY;
-const model = process.env.OPENAI_MODEL;
